d4e85ec604601ebaa40225f27210645815ce1ed4
Eric Horn <hornericr@me.com>
2021-07-28 22:45:27 -0700

    show

---
+++ 03-java-spring/02-spring-data-i/02-show/mvc/src/main/java/com/eric/mvc/controllers/BookController.java
@@ -0,0 +1,55 @@
+package com.eric.mvc.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eric.mvc.models.Book;
+import com.eric.mvc.services.BookService;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+    
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "/books/index.jsp";
+    }
+    
+    @RequestMapping("/books/{index}")
+    public String findBookByIndex(Model model, @PathVariable("index")Long id) {
+    	Book book = bookService.findBook(id);
+    	model.addAttribute("book", book);
+    	return "/books/show.jsp";
+    } 
+    
+	
+    @RequestMapping("/books/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "/books/new.jsp";
+    }
+    @RequestMapping(value="/books", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/books/new.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
+    }
+
+}
